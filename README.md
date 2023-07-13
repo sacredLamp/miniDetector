35d3006935ee173ad51b27be1aafd29592b319b8
ExplosiveEnd <henockdinberu11@gmail.com>
2023-07-13 00:41:43 -0400

    added start method + ping ability

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # miniDetector
-downDetector but scaled down to terminal
+downDetector but scaled down to terminal. so kawaiiii, right??
+
+The goal is to create a scaled-down version of the planned downDetector project
+to help understand what goes into making backend for this type of application, without having to worry about front-end elements.
--- main.cpp
+++ main.cpp
@@ -1,7 +1,78 @@
 #include <iostream>
 #include <algorithm>
 
+void start();
+
+void testPage(){
+    std::system("clear");
+    std::string input;
+    std::cout << "Enter website IP / URL ::\n";
+
+    while(true){
+
+        std::cin >> input;
+        if(input == "q")
+            break;
+
+        std::string cmd("ping " + input + " -c 1");
+
+        if(!std::system(cmd.c_str())) {
+            std::system("clear");
+            std::cout << "Website is up and running! ::\n";
+        }
+        else{
+            std::system("clear");
+            std::cout << "This webpage / server seems to be DOWN ::\n";
+        }
+
+        std::cout << "\nEnter 'q' to quit or enter another IP/URL ::\n\n";
+
+    }
+    start();
+}
+
+
+void start(){
+    std::system("clear");
+    char choice;
+
+    std::cout << "Welcome!\nInput your desired choice. ::\n";
+    std::cout << "1 -> Add Webpages ::\n";
+    std::cout << "2 -> View Favorites ::\n";
+    std::cout << "3 -> Test a Page ::\n";
+    std::cout << "q -> Exit ::\n";
+
+    while(choice != 'q') {
+        std::cin >> choice;
+        switch(choice){
+            case '1':
+                std::cout << "Add Webpages ::\n";
+                break;
+
+            case '2':
+                std::cout << "View Favorites ::\n";
+                break;
+
+            case '3':
+                testPage();
+                break;
+            case 'q':
+                std::cout << "Exit ::\n";
+                break;
+
+            default:
+                std::cout << "Not an option. ::\n";
+                break;
+
+        }
+    }
+}
+
+void run(){
+    start();
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    run();
     return 0;
 }
