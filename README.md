7d18d4a81d7435985d5729e4b69eb866a43c3078
ExplosiveEnd <henockdinberu11@gmail.com>
2023-07-16 23:53:53 -0400

    creates && r/w/a to .txt file

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # miniDetector
-downDetector but scaled down to terminal. so kawaiiii, right??
+downDetector but scaled down to terminal. So *kawaiiii*, right?
+miniDetector is a CLI application that allows you to check whether websites, databases, and services are currently online or unavailable, without having to search each one up individually.
+Chosen data is then saved into a `.txt` file, and saved so that on the next start-up, your list of sites remains intact. 
 
-The goal is to create a scaled-down version of the planned downDetector project
-to help understand what goes into making backend for this type of application, without having to worry about front-end elements.
+
+## Goal
+The goal is to create a scaled-down version of DownDetector in order to practice techniques that can be applied to the future project, without having to worry about UI elements. 
--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 #include <algorithm>
+#include <fstream>
 
 void start();
+
+void ping(std::string& input){
+
+    std::cout << "Pinging " << input << "...\n";
+    std::string cmd("ping " + input + " -c 1");
+
+    if(!std::system(cmd.c_str())) {
+        std::system("clear");
+        std::cout << "Website is up and running! ::\n";
+    }
+
+    else{
+        std::system("clear");
+        std::cout << "This webpage / server seems to be DOWN ::\n";
+    }
+
+}
 
 void testPage(){
     std::system("clear");
@@ -14,31 +32,90 @@ void testPage(){
         if(input == "q")
             break;
 
-        std::string cmd("ping " + input + " -c 1");
-
-        if(!std::system(cmd.c_str())) {
-            std::system("clear");
-            std::cout << "Website is up and running! ::\n";
-        }
-        else{
-            std::system("clear");
-            std::cout << "This webpage / server seems to be DOWN ::\n";
-        }
+        ping(input);
 
         std::cout << "\nEnter 'q' to quit or enter another IP/URL ::\n\n";
 
     }
+
+    start();
+}
+
+bool checkFileExists(std::fstream &file){
+    file.open("saved.txt", std::ios::in);
+    if(!file) {
+        std::cout << "File could not be created\n";
+        return false;
+    }
+    else{
+        //std::cout << "File created successfully\n";
+        file.close();
+        return true;
+    }
+
+}
+
+bool inFile(std::fstream &file, std::string &needle) {
+    std::string line;
+    file.open("saved.txt", std::ios::in);
+    if(file.is_open()) {
+        //std::cout << "File is open ::\n";
+        while (getline(file, line)) {
+            //std::cout << line << "\n";
+            if (line == needle) {
+                std::cout << "Connection is already saved ::\n\n";
+                return false;
+            }
+        }
+        file.close();
+        return true;
+    }
+    else{
+        std::cout << "File is not open ::\n";
+        return false;
+    }
+
+//return true;
+}
+
+void addPages(std::fstream& file){
+    std::system("clear");
+    std::string input;
+
+    if (checkFileExists(file)) {
+        while (input != "q") {
+            std::cout << "Input connection to save ::\n";
+            std::cin >> input;
+
+            if (input == "q") {
+                break;
+            }
+
+            //file.open("saved.txt", std::ios::in | std::ios::out | std::ios::app);
+            if (inFile(file, input)) {
+                file.open("saved.txt", std::ios::app);
+                std::cout << "Adding to list...\n";
+                file << input << "\n";
+                std::cout << "Connection added! ::\n\n";
+                file.close();
+            }
+
+        }
+    }
+
     start();
 }
 
 
 void start(){
+    std::fstream my_file;
+
     std::system("clear");
     char choice;
 
     std::cout << "Welcome!\nInput your desired choice. ::\n";
     std::cout << "1 -> Add Webpages ::\n";
-    std::cout << "2 -> View Favorites ::\n";
+    std::cout << "2 -> View Saved ::\n";
     std::cout << "3 -> Test a Page ::\n";
     std::cout << "q -> Exit ::\n";
 
@@ -47,6 +124,7 @@ void start(){
         switch(choice){
             case '1':
                 std::cout << "Add Webpages ::\n";
+                addPages(my_file);
                 break;
 
             case '2':
@@ -56,9 +134,10 @@ void start(){
             case '3':
                 testPage();
                 break;
+
             case 'q':
                 std::cout << "Exit ::\n";
-                break;
+                exit(0);
 
             default:
                 std::cout << "Not an option. ::\n";
